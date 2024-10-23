b1a15727e4b7258b90689db99c1252b1d2569210
Darik A O’Neil <darik.oneil.software@gmail.com>
2024-10-23 19:22:29 -0400

    re-added logging

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+from os import environ
+from pathlib import Path
+import numpy as np
+
+"""
+CONFIGURATION FOR TESTING
+
+"""
+
+# MANUALLY MAKE TEMP DIRECTORY SINCE PYTEST'S TMP_PATH IS SOLELY FUNCTION SCOPE
+_TEMPORARY_DIRECTORY = Path().cwd().joinpath("temp")
+if not _TEMPORARY_DIRECTORY.exists():
+    _TEMPORARY_DIRECTORY.mkdir(exist_ok=True)
+
+@pytest.fixture(scope="session")
+def temp_path():
+    return _TEMPORARY_DIRECTORY
