9b02131d4067beb98a87721da7a942d4003695c1
Darik A O’Neil <darik.oneil.software@gmail.com>
2024-10-23 19:31:58 -0400

    linting

--- exporgo/__init__.py
+++ exporgo/__init__.py
@@ -1,6 +1,13 @@
 from ._version import __current_version__, __package_name__
+from .experiment import Experiment
+from .files import FileTree
+from .subject import Subject
+
 
 __all__ = [
     "__current_version__",
     "__package_name__",
+    "Experiment",
+    "FileTree",
+    "Subject",
 ]
