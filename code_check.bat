5f0adcd17fdc8f6dbd308dac93692949151c021b
Darik A O’Neil <darik.oneil.software@gmail.com>
2024-10-23 13:38:11 -0400

    organizing package

---
+++ code_check.bat
@@ -0,0 +1,15 @@
+@echo off
+:: run test suite
+coverage run
+:: export coverage to json / lcov for processing
+coverage json
+coverage lcov
+:: export coverage to html for development in IDE
+coverage html
+:: report to console
+coverage report
+:: run linter (automatically goes to html for IDE due via configuration)
+flake8
+
+:: count scope of project
+cloc-1.98 --docstring-as-code ./exporgo ./docs ./tests --report-file=project_scope.md
