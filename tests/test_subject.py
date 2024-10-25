39538e7ea34819914b21d6c3b1ec2b0f15b6fb95
darikoneil <darik.oneil.software@gmail.com>
2024-10-25 18:16:26 -0400

    linting

--- tests/test_subject.py
+++ tests/test_subject.py
@@ -9,19 +9,20 @@ class TestSubject:
 
     def test_subject_initialization_with_valid_data(self, tmp_path):
         with patch("exporgo.subject.IPythonLogger.start_log") as mock_ipythonlogger:
-            subject = Subject(name="TestSubject",
+            subject = Subject(name="source",
                               directory=tmp_path,
                               species="Mouse",
                               study="Study1",
                               condition="Control",
                               meta = {"test": "details"},
                               extra = "extra details")
-            assert subject.name == "TestSubject"
-            assert subject.directory == tmp_path.joinpath("TestSubject")
+            assert subject.name == "source"
+            assert subject.directory == tmp_path.joinpath("source")
             assert subject.species == "Mouse"
             assert subject.study == "Study1"
             assert subject.condition == "Control"
             assert subject.meta == {"test": "details", "extra": "extra details"}
+            subject.index()
             assert mock_ipythonlogger.start_log.called_once()
 
     def test_subject_initialization_without_directory(self, tmp_path):
@@ -57,4 +58,17 @@ class TestSubject:
             subject._experiments["MockExperiment"] = "MockExperiment_"
             assert getattr(subject, "MockExperiment") == "MockExperiment_"
             assert subject.get("MockExperiment") == "MockExperiment_"
+            assert mock_ipythonlogger.start_log.called_once()
+
+    def test_subject_create_experiment(self, tmp_path):
+        with patch("exporgo.subject.IPythonLogger.start_log") as mock_ipythonlogger:
+            subject = Subject(name="TestSubject",
+                              directory=tmp_path,
+                              species="Mouse",
+                              study="Study1",
+                              condition="Control",
+                              meta = {"test": "details"},
+                              extra = "extra details")
+            subject.create_experiment("MockExperiment", "GenericExperiment")
+            assert "MockExperiment" in subject._experiments
             assert mock_ipythonlogger.start_log.called_once()
