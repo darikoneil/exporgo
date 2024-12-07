fc7c3013beea5d14eaf6beb39888be007e629910
Darik A O’Neil <darik.oneil.software@gmail.com>
2024-12-06 19:37:42 -0500

    re-added scheduler & format

--- exporgo/automation/scheduler.py
+++ exporgo/automation/scheduler.py
@@ -1,29 +1,28 @@
 import subprocess
 from pathlib import Path
+from task import Task
+from ..tools import convert, write_xml
+from ..types import File
+
+__all__ = ["schedule_task"]
 
 
-class Scheduler:
-    def __init__(self, config):
-        self.config = config
-        self._tmp = Path.cwd().joinpath("tmp")
-        self._tmp.mkdir()
-        self.file= None
-        self.task = ""
+def _construct_temporary_file(name: str) -> Path:
+    temporary_directory = Path.cwd().joinpath("tmp")
+    temporary_directory.mkdir(exist_ok=True)
+    temporary_file = temporary_directory.joinpath(name).with_suffix(".xml")
+    return temporary_file
 
-    def create(self):
-        ...
 
-    def delete(self):
-        ...
+@convert(parameter="file", permitted=(File, ), required=str)
+def _add_to_scheduler(name: str, file: File) -> bool:
+    command = ['schtasks', '/create', '/tn', name, '/xml', file]
+    result = subprocess.run(command, timeout=60, check=True, capture_output=True, text=True)
+    return result.returncode == 0
 
-    def _cli_execute(self):
-        cmd = ['schtasks', '/create', '/xml', self.file, '/tn', self.config]
-        result = subprocess.run(cmd, capture_output=True, text=True)
-        return result.stdout, result.stderr
 
-    def _serialize_task(self):
-        ...
-
-    # noinspection PyMethodMayBeStatic
-    def _create_task(self):
-        ...
+def schedule_task(task: Task) -> bool:
+    name = task.registration_info.name
+    file = _construct_temporary_file(name)
+    write_xml(file, task.model_dump_xml())
+    return _add_to_scheduler(name, file)
